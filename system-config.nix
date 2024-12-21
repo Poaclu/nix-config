89d717a3e2b4f42047cffcbf50529c158cb097d6
Poaclu <25772718+Poaclu@users.noreply.github.com>
2024-12-21 16:26:23 +0100

    corrected spacing

--- system-config.nix
+++ system-config.nix
@@ -16,11 +16,11 @@
   boot.loader = {
     systemd-boot.enable = false;
     efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        efiSupport = true;
-        devices = [ "nodev" ];
-      };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      devices = [ "nodev" ];
+    };
   };
 
   swapDevices = [{
