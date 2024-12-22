2a3f94251bcf3be6f091bbe08e74c3fb3fda4194
Poaclu <25772718+Poaclu@users.noreply.github.com>
2024-12-22 11:21:16 +0100

    adding gamescope

--- gaming.nix
+++ gaming.nix
@@ -1,9 +1,13 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
+    gamescope.enable = true;
   };
   environment.systemPackages = with pkgs; [
     protonup-qt
