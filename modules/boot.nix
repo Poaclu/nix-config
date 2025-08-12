172141201393053cea4ffd1f85571bb3daaa533c
Poaclu <25772718+Poaclu@users.noreply.github.com>
2025-08-12 16:49:00 +0200

    Add systemd-boot for futur lanzboot integration

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,37 +7,17 @@
 
 {
   options = {
-    grub = {
+    plymouth = {
       enable = lib.mkOption {
-        description = "Enable Grub config";
+        description = "Enable plymouth for a pretty boot";
         type = lib.types.bool;
         default = true;
         example = false;
       };
-      timeoutStyle = lib.mkOption {
-        default = "menu";
-        type = lib.types.enum [ "menu" "countdown" "hidden" ];
-        description = ''
-           - `menu` shows the menu.
-           - `countdown` uses a text-mode countdown.
-           - `hidden` hides GRUB entirely.'';
-      };
     };
   };
 
-  config = lib.mkIf config.grub.enable {
-    boot = {
-      loader = {
-        systemd-boot.enable = false;
-        grub = {
-          enable = true;
-          efiSupport = true;
-          efiInstallAsRemovable = true;
-          devices = [ "nodev" ];
-          timeoutStyle = config.grub.timeoutStyle;
-        };
-      };
-      plymouth.enable = true;
-    };
+  config = lib.mkIf config.plymouth.enable {
+    boot.plymouth.enable = true;
   };
 }
---
+++ modules/systemd-boot.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options = {
+    sysdb = {
+      enable = lib.mkOption {
+        description = "Enable Systemd-boot config";
+        type = lib.types.bool;
+        default = false;
+        example = true;
+      };
+    };
+  };
+
+  config = lib.mkIf config.sysdb.enable {
+    boot = {
+      loader = {
+        systemd-boot = {
+          enable = true;
+          configurationLimit = 10;
+        };
+      };
+    };
+  };
+}
