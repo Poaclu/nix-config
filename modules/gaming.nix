139ae664eab595b0cae730cda424a5cbf2f9cbc9
Poaclu <25772718+Poaclu@users.noreply.github.com>
2025-08-14 19:18:12 +0200

    Intergrate proton-ge packages in steam

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -41,6 +41,7 @@
     programs = {
       steam = {
         enable = true;
+        extraCompatPackages = with pkgs; [proton-ge-bin];
         gamescopeSession.enable = true;
         remotePlay.openFirewall = true;
       };
