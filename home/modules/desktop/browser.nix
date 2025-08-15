5fad695ccdb33c7cbaa3607c7c462312ec782060
Poaclu <25772718+Poaclu@users.noreply.github.com>
2025-08-15 23:16:43 +0200

    Sarting to configure zen-browser to be 100% declarable

--- home/modules/desktop/browser.nix
+++ home/modules/desktop/browser.nix
@@ -1,6 +1,30 @@
-{ config, pkgs, libs, inputs, ... }:
-{ 
-    home.packages = [
-      inputs.zen-browser.packages.${pkgs.system}.default
-    ];
+{ inputs, pkgs, ... }:
+let
+  zen = inputs.zen-browser.packages.${pkgs.system}.default;
+in {
+  programs.firefox = {
+    enable = true;
+    package = zen;                       # use Zen with the HM wrapper
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+
+    profiles.default = {
+      id = 0;
+      name = "default";
+      #extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        #privacy-badger
+      #];
+    };
+
+    policies = {
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = { installation_mode = "force_installed"; };
+        "bitwarden-password-manager@bitwarden.com" = { installation_mode = "allowed"; };
+      };
+    };
+  };
+
+  # Remove any Zen entry here. Let the module handle it.
+  home.packages = [ ];
 }
