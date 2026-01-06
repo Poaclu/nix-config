41025f79d4295c1cfa718ee342203219797bbb39
Poaclu <25772718+Poaclu@users.noreply.github.com>
2026-01-06 23:04:11 +0100

    add identity path to force agenix to look at a certain ssh file

--- NixOS/common/agenix.nix
+++ NixOS/common/agenix.nix
@@ -3,7 +3,13 @@
   ...
 }:
 {
-  age.secrets = {
-    tailscale.file = "${inputs.secrets}/tailscale.age";
+  age = {
+    secrets = {
+      tailscale.file = "${inputs.secrets}/tailscale.age";
+    };
+    identityPaths = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/home/poaclu/.ssh/id_ed25519_agenix"
+    ];
   };
 }
