b47deac3f28fe28be17ef46742bb88bb2840d370
Poaclu <25772718+Poaclu@users.noreply.github.com>
2026-01-10 14:29:09 +0100

    Add subtituters as trusted also + formating

--- Common/nix.nix
+++ Common/nix.nix
@@ -8,23 +8,29 @@
       trusted-users = [
         "poaclu"
         "root"
+        "@wheel"
       ];
       substituters = [
-
-      "https://cache.nixos.org/"
-      "https://cachix.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://nixpkgs.cachix.org"
-      "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+        "https://cachix.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-substituters = [
+        "https://cache.nixos.org/"
+        "https://cachix.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
