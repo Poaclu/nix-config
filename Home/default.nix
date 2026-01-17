421b3298ab18564bf3c2954214a98cad03b9905f
Poaclu <25772718+Poaclu@users.noreply.github.com>
2026-01-17 17:24:09 +0100

    Fix home-manager for non-NixOS setups

--- Home/default.nix
+++ Home/default.nix
@@ -12,10 +12,17 @@
 self,
 ...
 }:
+let
+  system = "x86_64-linux";
+  pkgs = import self.inputs.nixpkgs {
+    inherit system;
+  };
+in
 {
   flake = {
     homeConfigurations = {
       poaclu = self.inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
         modules = [
           ./shell
           ./main.nix
