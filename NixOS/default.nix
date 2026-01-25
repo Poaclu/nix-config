5bbd4009c92736b6453dfc0162dd1912c08b294f
Poaclu <25772718+Poaclu@users.noreply.github.com>
2026-01-25 18:27:21 +0100

    Refactor flake.nixosConfigurations file; Heavily inspired byt nothebee https://git.notthebe.ee/notthebee/nix-config/src/branch/main/modules/machines/nixos/default.nix

--- NixOS/default.nix
+++ NixOS/default.nix
@@ -1,165 +1,84 @@
 {
+  lib,
   self,
   ...
-  }:
-  {
-  flake = {
-    nixosConfigurations = {
-      killi = self.inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit self;
-        };
-        modules = [
-          ./hosts/killi/configuration.nix
-          ({ pkgs, ... }: {
-            imports = [
-              ./modules/common
-              ./modules/desktop
-              ../common
-              self.inputs.agenix.nixosModules.default
-              self.inputs.play.nixosModules.play
-              self.inputs.lanzaboote.nixosModules.lanzaboote
-              self.inputs.disko.nixosModules.disko
-              self.inputs.home-manager.nixosModules.home-manager {
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.extraSpecialArgs = {
-                  inherit (self) inputs;
-                };
-                home-manager.users.poaclu.imports = [
-                  self.inputs.zen-browser.homeModules.beta
-                  self.inputs.nvf.homeManagerModules.default
-                  ../Home/shell
-                  ../Home/desktop
-                  ../Home/main.nix
-                  ../Home/user.nix
-                  ../common
-                  ./hosts/killi/home.nix
-                ];
-              }
-            ];
-          })
-        ];
-      };
-      kermel = self.inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit self;
-        };
-        modules = [
-          ./hosts/kermel/configuration.nix
-          ({ pkgs, ... }: {
-            imports = [
-              ./modules/common
-              ./modules/desktop
-              ../common
-              self.inputs.agenix.nixosModules.default
-              self.inputs.play.nixosModules.play
-              self.inputs.lanzaboote.nixosModules.lanzaboote
-              self.inputs.disko.nixosModules.disko
-              self.inputs.home-manager.nixosModules.home-manager {
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.extraSpecialArgs = {
-                  inherit (self) inputs;
-                };
-                home-manager.users.poaclu.imports = [
-                  self.inputs.zen-browser.homeModules.beta
-                  self.inputs.nvf.homeManagerModules.default
-                  ../Home/shell
-                  ../Home/desktop
-                  ../Home/main.nix
-                  ../Home/user.nix
-                  ../common
-                ];
-              }
-            ];
-          })
-        ];
-      };
-      odonata = self.inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit self;
-        };
-        modules = [
-          ./hosts/odonata/configuration.nix
-          ({ pkgs, ... }: {
-            imports = [
-              (import "${self.inputs.mobile-nixos}/lib/configuration.nix" { device = "oneplus-enchilada"; })
-              ./modules/common
-              ./modules/desktop
-              ../common
-              self.inputs.agenix.nixosModules.default
-              self.inputs.play.nixosModules.play
-              self.inputs.lanzaboote.nixosModules.lanzaboote
-              self.inputs.disko.nixosModules.disko
-              self.inputs.home-manager.nixosModules.home-manager {
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.extraSpecialArgs = {
-                  inherit (self) inputs;
-                };
-                home-manager.users.poaclu.imports = [
-                  self.inputs.zen-browser.homeModules.beta
-                  self.inputs.nvf.homeManagerModules.default
-                  ../Home/shell
-                  ../Home/desktop
-                  ../Home/main.nix
-                  ../Home/user.nix
-                  ../common
-                  ./hosts/odonata/home.nix
-                ];
-              }
-            ];
-          })
-        ];
-      };
-      coruscant = self.inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit self;
-        };
-        modules = [
-          ./hosts/coruscant/configuration.nix
-          ({ pkgs, ... }: {
-            imports = [
-              ./modules/common
-              ./modules/desktop
-              ../common
-              self.inputs.agenix.nixosModules.default
-              self.inputs.play.nixosModules.play
-              self.inputs.lanzaboote.nixosModules.lanzaboote
-              self.inputs.disko.nixosModules.disko
-              self.inputs.home-manager.nixosModules.home-manager {
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.extraSpecialArgs = {
-                  inherit (self) inputs;
-                };
-                home-manager.users.poaclu.imports = [
-                  self.inputs.zen-browser.homeModules.beta
-                  self.inputs.nvf.homeManagerModules.default
-                  ../Home/shell
-                  ../Home/desktop
-                  ../Home/main.nix
-                  ../Home/user.nix
-                  ../common
-                  ./hosts/coruscant/home.nix
-                ];
-              }
-            ];
-          })
-        ];
-      };
-      #homeConfigurations = {
-      #  poaclu = inputs.home-manager.lib.homeManagerConfiguration {
-      #    modules = [
-      #     ../Home/shell
-      #     ../Home/main.nix
-      #     ../Home/user.nix
-      #     ../common
-      #     inputs.nvf.homeManagerModules.default
-      #   ];
-      # };
-      #};
+}:
+let
+  entries = builtins.attrNames (builtins.readDir ./hosts/.);
+  configs = builtins.filter (dir: builtins.pathExists (./hosts/. + "/${dir}/configuration.nix")) entries;
+  homeManagerCfg = name: userPackages: extraImports: {
+    home-manager.useGlobalPkgs = false;
+    home-manager.useUserPackages = userPackages;
+    home-manager.backupFileExtension = "bak";
+    home-manager.extraSpecialArgs = {
+      inherit (self) inputs;
     };
+    home-manager.users.poaclu.imports = [
+      self.inputs.zen-browser.homeModules.beta
+      self.inputs.nvf.homeManagerModules.default
+      ../Home/shell
+      ../Home/desktop
+      ../Home/main.nix
+      ../Home/user.nix
+      ../common
+    ]
+    ++ (if builtins.pathExists (./hosts/. + "/${name}/home.nix")
+        then [ (./hosts/. + "/${name}/home.nix") ]
+        else [])
+    ++ extraImports;
   };
+in
+{
+  flake.nixosConfigurations = 
+      let
+        nixpkgsMap = {
+          killi = ""; #"-unstable";
+        };
+        systemArchMap = {
+          odonata = "aarch64-linux";
+        };
+        myNixosSystem =
+          name: self.inputs."nixpkgs${lib.attrsets.attrByPath [ name ] "" nixpkgsMap}".lib.nixosSystem;
+
+        mobileDeviceMap = {
+          odonata = [
+            (
+              import (self.inputs.mobile-nixos + "/lib/configuration.nix") {
+                device = "oneplus-enchilada"; 
+              }
+            )
+          ];
+        };
+        extraModulesFor = 
+          name: lib.attrsets.attrByPath [ name ] [ ] mobileDeviceMap;
+      in
+      lib.listToAttrs (
+        builtins.map (
+          name:
+          lib.nameValuePair name (
+            (myNixosSystem name) {
+              system = lib.attrsets.attrByPath [ name ] "x86_64-linux" systemArchMap;
+              specialArgs = {
+                inherit self;
+              };
+              
+              modules = [
+                ./modules/common
+                ./modules/desktop
+                ../common
+                self.inputs.agenix.nixosModules.default
+                self.inputs.play.nixosModules.play
+                self.inputs.lanzaboote.nixosModules.lanzaboote
+                self.inputs.disko.nixosModules.disko
+                self.inputs."home-manager${
+                  lib.attrsets.attrByPath [ name ] "" nixpkgsMap
+                }".nixosModules.home-manager
+                (./hosts/. + "/${name}/configuration.nix")
+                (homeManagerCfg name false [ ])
+              ]
+              ++ extraModulesFor name;
+            }
+          )
+        ) configs
+      );
 }
